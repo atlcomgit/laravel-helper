7ea9adbae4bcdda862d1ffac84ec301de8754301
Alek <alek@atlcom.ru>
2025-09-11 12:02:14 +0700

    update HttpLogRepository::create

--- src/Repositories/HttpLogRepository.php
+++ src/Repositories/HttpLogRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Atlcom\LaravelHelper\Repositories;
 
+use Atlcom\Hlp;
 use Atlcom\LaravelHelper\Defaults\DefaultRepository;
 use Atlcom\LaravelHelper\Dto\HttpLogCreateDto;
 use Atlcom\LaravelHelper\Dto\HttpLogFailedDto;
@@ -11,6 +12,7 @@ use Atlcom\LaravelHelper\Dto\HttpLogUpdateDto;
 use Atlcom\LaravelHelper\Enums\ConfigEnum;
 use Atlcom\LaravelHelper\Facades\Lh;
 use Atlcom\LaravelHelper\Models\HttpLog;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @internal
@@ -33,12 +35,30 @@ class HttpLogRepository extends DefaultRepository
      */
     public function create(HttpLogCreateDto $dto): void
     {
-        $this->withoutTelescope(
-            fn () => $this->model::query()
-                ->withoutQueryLog()
-                ->withoutQueryCache()
-                ->create($dto->toArray())
-        );
+        $this->withoutTelescope(function () use ($dto) {
+            $this->model = Lh::config(ConfigEnum::HttpLog, 'model') ?? HttpLog::class;
+
+            $config = ConfigEnum::HttpLog;
+            $connection = Lh::getConnection($config);
+            $table = Lh::getTable($config);
+            $data = $dto->serializeKeys(['type', 'method', 'status'])->toArray();
+            $data['created_at'] = now()->toDateTimeString();
+            $data['updated_at'] = $data['created_at'];
+            $data = array_map(
+                static fn ($item) => is_array($item) ? Hlp::castToJson($item) : $item,
+                $data,
+            );
+
+            $columns = array_keys($data);
+            $placeholders = implode(', ', array_fill(0, count($columns), '?'));
+            $columnsSql = implode(', ', array_map(static fn (string $c): string => "`$c`", $columns));
+
+            DB::connection($connection)
+                ->statement(
+                    "insert into {$table} ({$columnsSql}) values ({$placeholders})",
+                    array_values($data),
+                );
+        });
     }
 
 
